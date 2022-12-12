b7dae08d7c38911fa874c2adc33dde0f19f879c6
suhye0n <contact.suhyeon@gmail.com>
2022-12-12 13:26:16 +0900

    propertychanged 구현

--- Week15Canvas/MainWindow.xaml.cs
+++ Week15Canvas/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,26 @@ namespace Week15Canvas
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public double PosX { get; set; }
+        public double PosY { get; set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+            PosX = 200;
+            PosY = 200;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string proportyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(proportyName));
         }
     }
 }
